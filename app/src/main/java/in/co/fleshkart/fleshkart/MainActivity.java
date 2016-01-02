a1f6fe8bc3bcf4ac99549ce0133415da26fbfabf
gokul <rbp687@motorola.com>
2016-01-02 11:37:42 +0530

    Saranya demo purpose

--- app/src/main/java/in/co/fleshkart/fleshkart/MainActivity.java
+++ app/src/main/java/in/co/fleshkart/fleshkart/MainActivity.java
@@ -11,6 +11,7 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.util.Pair;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -18,6 +19,8 @@ import android.view.View;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+
+    public static String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -98,6 +101,7 @@ public class MainActivity extends AppCompatActivity
         } else if (id == R.id.nav_send) {
             Intent intent = new Intent(this, MapsActivity.class);
             this.startActivity(intent);
+            Log.d(TAG, "Nav_send is cliecked");
 
         }
 
