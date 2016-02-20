eef00bc0829c9c61aac1a4ba333ae3e0cec5d300
Saranyagokul <j.saranyabtech@gmail.com>
2016-02-20 18:43:35 +0530

    test git

--- app/src/main/java/in/co/fleshkart/fleshkart/MapsActivity.java
+++ app/src/main/java/in/co/fleshkart/fleshkart/MapsActivity.java
@@ -17,6 +17,7 @@ public class MapsActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+		//Checking git
         setContentView(R.layout.activity_maps);
         final ActionBar actionBar = getActionBar();
         if (actionBar != null) {
